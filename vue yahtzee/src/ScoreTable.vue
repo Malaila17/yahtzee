24f11caf16443da6218fb24901b482b1694368b1
Wieke <68997647+Malaila17@users.noreply.github.com>
2025-10-09 11:16:32 +0200

    added scoring part 2

--- vue yahtzee/src/ScoreTable.vue
+++ vue yahtzee/src/ScoreTable.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, computed} from 'vue';
+import {computed} from 'vue';
 const dice = defineModel();
 
 const count = computed(() => {
@@ -22,7 +22,77 @@ const sumOfDice = computed(() => {
     return dice.value.reduce((sum, die) => sum + die, 0);
 });
 
+const checkSame =(numSame) => {
+    for (let num in count.value) {
+        if (count.value[num] >= numSame) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const checkStreet = (streetLength) => {
+    let street = [];
+    for (let num in count.value) {
+        if (count.value[num] != 0) {
+        street.push(num);
+        } else {
+        if (street.length < streetLength) {
+            street = [];
+        }
+        }
+    }
+    if (street.length >= streetLength) {
+        return true;
+    }
+    return false;
+}
+
+const threeOfAKind = computed(() => checkSame(3) ? sumOfDice : 0);
+const carre = computed(() => checkSame(4) ? sumOfDice : 0);
+const yahtzee = computed(() => checkSame(5) ? 50 : 0);
+const kleineStraat = computed(() => checkStreet(4) ? 30 : 0);
+const groteStraat = computed(() => checkStreet(5) ? 40 : 0);
+const fullHouse = computed(() => {
+    for (let num in count.value) {
+        if (count.value[num] == 1 || count.value[num] == 4 || count.value[num == 5]) {
+        return 0;
+        }
+    }
+    return 25;
+});
+
+const scoresPart2 = [threeOfAKind, carre, fullHouse, kleineStraat, groteStraat, yahtzee, sumOfDice];
+
 const numerals = ['enen', 'tweeën', 'drieën', 'vieren', 'vijven', 'zessen'];
+const namesPart2 = [
+    "Three of a kind",
+    "Carré ",
+    "Full house ",
+    "Kleine straat",
+    "Grote straat",
+    "Yahtzee ",
+    "Chance "
+]
+const explanationsPart2 = [
+    "3 dezelfde",
+    "4 dezelfde",
+    "3+2 dezelfde",
+    "4 opeenvolgend",
+    "5 opeenvolgend",
+    "5 dezelfde",
+    "vrije keus"
+]
+const scoreGuidePart2 = [
+    "Totaal v.d 5 stenen",
+    "Totaal v.d 5 stenen",
+    "25 pt",
+    "30 pt",
+    "40 pt",
+    "50 pt",
+    "Totaal v.d 5 stenen",
+]
+
 </script>
 
 <template>
@@ -48,7 +118,7 @@ const numerals = ['enen', 'tweeën', 'drieën', 'vieren', 'vijven', 'zessen'];
         <tr>
             <td>
                 Bonus
-                <br />
+                <br>
                 <i>bij 63 of meer</i>
             </td>
             <td class="puntentelling">35 pt</td>
@@ -65,75 +135,11 @@ const numerals = ['enen', 'tweeën', 'drieën', 'vieren', 'vijven', 'zessen'];
             <td></td>
             <td></td>
         </tr>
-        <tr>
-            <td class="deel2 worp">
-                Three of a kind
-                <br />
-                <i>3 dezelfde</i>
-            </td>
-            <td class="deel2 puntentelling">Totaal v.d 5 stenen</td>
-            <td class="deel2 score spel1"></td>
-            <td class="deel2 score spel2"></td>
-        </tr>
-        <tr>
-            <td class="deel2 worp">
-                Carré
-                <br />
-                <i>4 dezelfde</i>
-            </td>
-            <td class="deel2 puntentelling">Totaal v.d 5 stenen</td>
-            <td class="deel2 score spel1"></td>
-            <td class="deel2 score spel2"></td>
-        </tr>
-        <tr>
-            <td class="deel2 worp">
-                Full house
-                <br />
-                <i>3+2 dezelfde</i>
-            </td>
-            <td class="deel2 puntentelling">25 pt</td>
-            <td class="deel2 score spel1"></td>
-            <td class="deel2 score spel2"></td>
-        </tr>
-        <tr>
-            <td class="deel2 worp">
-                Kleine straat
-                <br />
-                <i>4 opeenvolgend</i>
-            </td>
-            <td class="deel2 puntentelling">30 pt</td>
-            <td class="deel2 score spel1"></td>
-            <td class="deel2 score spel2"></td>
-        </tr>
-        <tr>
-            <td class="deel2 worp">
-                Grote straat
-                <br />
-                <i>5 opeenvolgend</i>
-            </td>
-            <td class="deel2 puntentelling">40 pt</td>
-            <td class="deel2 score spel1"></td>
-            <td class="deel2 score spel2"></td>
-        </tr>
-        <tr>
-            <td class="deel2 worp">
-                Yahtzee
-                <br />
-                <i>5 dezelfde</i>
-            </td>
-            <td class="deel2 puntentelling">50 pt</td>
-            <td class="deel2 score spel1"></td>
-            <td class="deel2 score spel2"></td>
-        </tr>
-        <tr>
-            <td class="deel2 worp">
-                Chance
-                <br />
-                <i>vrije keus</i>
-            </td>
-            <td class="deel2 puntentelling">Totaal v.d 5 stenen</td>
-            <td class="deel2 score spel1"></td>
-            <td class="deel2 score spel2"></td>
+        <tr v-for="(score, index) in scoresPart2" :key="index">
+            <td>{{ namesPart2[index] }}<br><i>{{ explanationsPart2[index] }}</i></td>
+            <td>{{ scoreGuidePart2[index] }}</td>
+            <td>{{ scoresPart2[index] }}</td>
+            <td></td>
         </tr>
         <tr>
             <td colspan="2">Totaal onderste helft</td>
